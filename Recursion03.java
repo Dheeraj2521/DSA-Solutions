2e1a01976c982c19b77cea16ca2dce60bfbfd32c
Dheeraj2521 <polemdheeraj20@gmail.com>
2025-12-29 21:31:19 +0530

    Create Recursion03.java

---
+++ Recursion03.java
@@ -0,0 +1,14 @@
+public class Recursion03 {
+
+    static int sum(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return n + sum(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println("Sum = " + sum(num));
+    }
+}
