61c207a8e792f659597526075e7bf26bb956f4a7
Dheeraj2521 <polemdheeraj20@gmail.com>
2025-12-29 21:20:03 +0530

    Create String02.java

---
+++ String02.java
@@ -0,0 +1,10 @@
+public class String02 {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+        String upper = str.toUpperCase();
+        String lower = str.toLowerCase();
+
+        System.out.println("Uppercase: " + upper);
+        System.out.println("Lowercase: " + lower);
+    }
+}
