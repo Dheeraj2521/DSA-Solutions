57c1dd04af62675c2517f3d8e7de8c9c16348e8f
Dheeraj2521 <polemdheeraj20@gmail.com>
2025-12-29 21:40:22 +0530

    Create Search01.java

---
+++ Search01.java
@@ -0,0 +1,28 @@
+public class Search01 {
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 8, 10};
+        int key = 6;
+        int low = 0;
+        int high = arr.length - 1;
+        boolean found = false;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == key) {
+                found = true;
+                break;
+            } else if (arr[mid] < key) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        if (found) {
+            System.out.println("Element found");
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
