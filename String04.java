0475a84fbb7b510644ceb58c918e69ac17c27fe0
Dheeraj2521 <polemdheeraj20@gmail.com>
2025-12-29 21:20:59 +0530

    Create String04.java

---
+++ String04.java
@@ -0,0 +1,18 @@
+public class String04 {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        int vowels = 0;
+
+        str = str.toLowerCase();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || 
+                ch == 'o' || ch == 'u') {
+                vowels++;
+            }
+        }
+
+        System.out.println("Number of vowels = " + vowels);
+    }
+}
