1fddd09d58369a14b015c6701cfc1435c9eacda1
Dheeraj2521 <polemdheeraj20@gmail.com>
2025-12-29 21:18:34 +0530

    Create String01.java

---
+++ String01.java
@@ -0,0 +1,8 @@
+public class String01 {
+    public static void main(String[] args) {
+        String str = "hello world";
+        int length = str.length();
+
+        System.out.println("Length of string = " + length);
+    }
+}
