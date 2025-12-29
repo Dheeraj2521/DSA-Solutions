ae8cc15b5a8ba2f7f38667f9aaa0729351f1b341
Dheeraj2521 <polemdheeraj20@gmail.com>
2025-12-29 21:31:59 +0530

    Create Recursion04.java

---
+++ Recursion04.java
@@ -0,0 +1,17 @@
+public class Recursion04 {
+
+    static int fibonacci(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int n = 6;
+        System.out.print("Fibonacci series: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+}
