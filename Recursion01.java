4aa4639d20f22288d4d93186c5868e98893af4bc
Dheeraj2521 <polemdheeraj20@gmail.com>
2025-12-29 21:28:23 +0530

    Create Recursion01.java

---
+++ Recursion01.java
@@ -0,0 +1,14 @@
+public class Recursion01 {
+
+    static void printNumbers(int n) {
+        if (n == 0) {
+            return;
+        }
+        printNumbers(n - 1);
+        System.out.print(n + " ");
+    }
+
+    public static void main(String[] args) {
+        printNumbers(5);
+    }
+}
