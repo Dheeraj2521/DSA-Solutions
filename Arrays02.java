37fb8bec89d187ce980e34cc3ca4a2959aa1525a
Dheeraj2521 <polemdheeraj20@gmail.com>
2025-12-29 21:10:02 +0530

    Create Arrays02.java

---
+++ Arrays02.java
@@ -0,0 +1,14 @@
+public class Arrays02 {
+    public static void main(String[] args) {
+        int[] arr = {12, 5, 8, 20, 3};
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Maximum element = " + max);
+    }
+}
