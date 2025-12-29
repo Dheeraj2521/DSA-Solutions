8c0278c9c8960c359924ba9c7af6316658e3f3cc
Dheeraj2521 <polemdheeraj20@gmail.com>
2025-12-29 21:10:39 +0530

    Create Arrays03.java

---
+++ Arrays03.java
@@ -0,0 +1,14 @@
+public class Arrays03 {
+    public static void main(String[] args) {
+        int[] arr = {4, 7, 1, 9, 2};
+        int min = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println("Minimum element = " + min);
+    }
+}
